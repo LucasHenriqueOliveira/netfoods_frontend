8e58cc47a21521efb9a9bce2c37b94d5cb7056b2
Lucas <lucashen@gmail.com>
2017-09-14 19:42:54 -0300

    content right (pedidos)

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,13 +2,54 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+	selector: 'page-home',
+	templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+	pedidos = [{
+		number: "ORD.048922",
+		value: "R$4.500,00",
+		date: "28/08",
+		client: "Temakeria"
+	},
+	{
+		number: "ORD.048723",
+		value: "R$15.500,00",
+		date: "27/08",
+		client: "Paris 6"
+	},
+	{
+		number: "ORD.048654",
+		value: "R$3.500,00",
+		date: "27/08",
+		client: "Grand Cru"
+	},
+	{
+		number: "ORD.048723",
+		value: "R$24.000,00",
+		date: "28/08",
+		client: "Jam"
+	},
+	{
+		number: "ORD.048777",
+		value: "R$3.500,00",
+		date: "27/08",
+		client: "Grand Cru"
+	}, 
+	{
+		number: "ORD.048796",
+		value: "R$24.500,00",
+		date: "28/08",
+		client: "Jam"
+	}]
+	
+	constructor(public navCtrl: NavController) {
 
-  }
+	}
+
+	go() {
+		
+	}
 
 }
