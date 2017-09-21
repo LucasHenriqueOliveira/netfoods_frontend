97b94fd2f019ba445a08c549f5bf77df66ef3318
Lucas <lucashen@gmail.com>
2017-09-20 21:03:59 -0300

    field search (Busca) in products

--- src/components/products/products.ts
+++ src/components/products/products.ts
@@ -14,17 +14,46 @@ import { PopoverComponent } from '../popover/popover';
 })
 export class ProductsComponent {
 
-  @Input() products: any
+	@Input() products: any
+	private items: any;
+	private inputItem: string = '';
+	private qtd_items: number;
 
-  constructor(private popoverCtrl: PopoverController) {
-    
-  }
+	constructor(private popoverCtrl: PopoverController) {
+		
+	}
 
-  presentPopover(ev) {
-    let popover = this.popoverCtrl.create(PopoverComponent, { menu: 'teste'});
-    popover.present({
-      ev: ev
-    });
-  }
+	ngOnInit() {
+		this.initializeItems();
+	}
+
+	initializeItems() {
+		this.items = this.products;
+		this.qtd_items = this.items.length;
+	}
+
+	getItems(ev: any) {
+
+		// Reset items back to all of the items
+		this.initializeItems();
+	
+		// set val to the value of the searchbar
+		let val = ev;
+	
+		// if the value is an empty string don't filter the items
+		if (val && val.trim() != '') {
+		  this.items = this.items.filter((item) => {
+			return (item.name.toLowerCase().indexOf(val.toLowerCase()) > -1);
+		  })
+		  this.qtd_items = this.items.length;
+		}
+	}
+
+	presentPopover(ev) {
+		let popover = this.popoverCtrl.create(PopoverComponent, { menu: 'teste'});
+			popover.present({
+			ev: ev
+		});
+	}
 
 }
