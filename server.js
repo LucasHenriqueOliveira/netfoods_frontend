b625594a5ef001d79d1e9a108edde95f36d4bef1
Lucas <lucashen@gmail.com>
2017-10-01 14:58:09 -0300

    change server.js

--- server.js
+++ server.js
@@ -2,7 +2,15 @@ const
     express = require('express'),
     app = express();
 
-app.use(express.static('www'))
+app.use((req, res, next) => {
+    if (req.headers['x-forwarded-proto'] != 'https') {
+        res.redirect('https://' + req.headers.host + req.url)
+    } else {
+        next()
+    }
+});
+
+app.use(express.static('www'));
 
 const port = process.env.PORT || 7000;
 app.listen(port, () => {
