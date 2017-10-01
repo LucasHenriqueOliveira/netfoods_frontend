f9096b120a0db9669c3a9283ba2f3e9ad17748be
Lucas <lucashen@gmail.com>
2017-10-01 15:08:04 -0300

    remove www in gitignore

--- server.js
+++ server.js
@@ -1,16 +1,7 @@
-const
-    express = require('express'),
-    app = express();
+var express = require('express'),
+app = express();
 
-app.use((req, res, next) => {
-    if (req.headers['x-forwarded-proto'] != 'https') {
-        res.redirect('https://' + req.headers.host + req.url)
-    } else {
-        next()
-    }
-});
-
-app.use(express.static('www'));
+app.use(express.static('www'))
 
 const port = process.env.PORT || 7000;
 app.listen(port, () => {
